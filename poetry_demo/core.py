f26542c56b8e734959e3528ecf3cbe452b68af85
Wai Kiat Tan <wk.tan@outlook.com>
2022-11-03 15:20:33 +0800

    fix: remove func c

--- poetry_demo/core.py
+++ poetry_demo/core.py
@@ -6,10 +6,6 @@ def get_pulsifi_tech_team():
     return ["data-team", "app-team", "product-team", "stackholder-team"]
 
 
-def return_c():
-    return "c"
-
-
 def i_dont_know():
     return "I dont know what the hell is going on"
 
